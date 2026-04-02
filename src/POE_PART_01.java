0d6b43958ea2ba1ddd85ef252c53fbb6037fd50f
Student <Student@LabVM2047644.caz3htdjbkjejfs34ifoyahfsd.jnbx.internal.cloudapp.net>
2026-04-02 10:36:17 +0200

    add a greeting

--- src/POE_PART_01.java
+++ src/POE_PART_01.java
@@ -13,6 +13,7 @@ public class POE_PART_01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hello Git");
         // TODO code application logic here
     }
     
